8e058dc90ef4b5ad1a2051362ea55e539b8b5632
aksen <aksenov.andrey26@gmail.com>
2025-09-05 16:58:48 +0400

    separated ui logic in new file ui.go

--- main.go
+++ main.go
@@ -6,9 +6,6 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/jedib0t/go-pretty/v6/text"
 )
 
 func main() {
@@ -26,18 +23,6 @@ func main() {
 	ticker := time.NewTicker(3 * time.Second)
 	done := make(chan os.Signal)
 	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
-
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{
-		text.Colors{text.FgHiCyan, text.Bold}.Sprint("URL"),
-		text.Colors{text.FgHiCyan, text.Bold}.Sprint("Status Code"),
-	})
-	t.SetStyle(table.StyleColoredBright)
-
-	colorKey := text.Colors{text.FgYellow, text.BgWhite}
-	colorValue := text.Colors{text.FgGreen, text.BgWhite}
-	color404 := text.Colors{text.FgRed, text.BgWhite}
 	defer ticker.Stop()
 	for {
 		select {
@@ -45,23 +30,9 @@ func main() {
 			fmt.Println("Exiting...")
 			return
 		case <-ticker.C:
-			t.ResetRows()
 			data = queryManager(urls)
-			for k, v := range data {
-				cKey := colorKey.Sprint(k)
-				if isRightCode(v) {
+			drawTable(data)
 
-					cValue := colorValue.Sprint(v)
-					colorValue.Sprint(v)
-					t.AppendRow(table.Row{cKey, cValue})
-				} else {
-					c404 := color404.Sprint("404 Not Found")
-					t.AppendRow(table.Row{cKey, c404})
-				}
-
-			}
-			fmt.Print("\033[H\033[2J")
-			t.Render()
 		}
 	}
 
---
+++ ui.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
+)
+
+func drawTable(data map[string]int) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+
+	// Важно: задаём стиль с цветами вручную!
+	style := table.StyleColoredBright
+	style.Color = table.ColorOptions{
+		Header:       text.Colors{text.FgMagenta, text.Bold},
+		Row:          text.Colors{text.FgGreen},
+		RowAlternate: text.Colors{text.FgGreen},
+		Footer:       text.Colors{text.FgMagenta, text.Bold},
+	}
+	t.SetStyle(style)
+
+	t.AppendHeader(table.Row{
+		text.Colors{text.FgMagenta, text.Bold}.Sprint("URL"),
+		text.Colors{text.FgMagenta, text.Bold}.Sprint("Status Code"),
+	})
+
+	colorKey := text.Colors{text.FgYellow}
+	colorValue := text.Colors{text.FgGreen}
+	color404 := text.Colors{text.FgRed}
+
+	for k, v := range data {
+		cKey := colorKey.Sprint(k)
+		if isRightCode(v) {
+			t.AppendRow(table.Row{cKey, colorValue.Sprint(v)})
+		} else {
+			t.AppendRow(table.Row{cKey, color404.Sprint("404 Not Found")})
+		}
+	}
+
+	fmt.Print("\033[H\033[2J") // Очистка экрана (опционально)
+	t.Render()
+}
